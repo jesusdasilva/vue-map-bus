93927be9b63393d8164c5138c5cf1dac95cc0593
Jesús Manuel Dasilva Barreto <jesusdasilva@gmail.com>
2023-05-29 09:19:52 -0400

    agregar comentarios

--- src/components/MapBus.vue
+++ src/components/MapBus.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted, onUnmounted,  watchEffect } from "vue";
-import { GoogleMap, CustomMarker } from "vue3-google-map";
+import { GoogleMap, CustomMarker } from "vue3-google-map"; // Componentes para vue 3 de Google Maps, lo conseguí en internet
 
 const positionBus = ref({ lat: 0, lng: 0 });
 const ws = ref(null);
@@ -65,19 +65,21 @@ onUnmounted(() => {
 </script>
 
 <template>
+    <!-- Componente que dibuja el Mapa -->
   <GoogleMap
     api-key="AIzaSyDHNeCjHnNbwyLMkPTIsGEBJQ_1AjxMCz8"
     style="width: 100%; height: 500px"
     :center="positionBus"
     :zoom="15"
   >
+    <!-- Componente que dibuja el marcador del Bus -->
     <CustomMarker :options="{ position: positionBus, anchorPoint: 'BOTTOM_CENTER' }">
+        <!-- Aquí puedes poner el código HTML que quieras para el marcador -->
       <div style="width: 30px; height: 30px;">
         <span style="background-color: aqua;">10180276</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 0C422.4 0 512 35.2 512 80V96l0 32c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32l0 160c0 17.7-14.3 32-32 32v32c0 17.7-14.3 32-32 32H416c-17.7 0-32-14.3-32-32V448H192v32c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32l0-32c-17.7 0-32-14.3-32-32l0-160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h0V96h0V80C64 35.2 153.6 0 288 0zM128 160v96c0 17.7 14.3 32 32 32H272V128H160c-17.7 0-32 14.3-32 32zM304 288H416c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H304V288zM144 400a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm288 0a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM384 80c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16z"/></svg>
       </div>
     </CustomMarker>
-
   </GoogleMap>
 
 </template>
